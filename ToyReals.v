53ce74474517e0672806449057f12f7b90bb833f
ccd0 <admin@containerchan.org>
2021-04-23 12:59:49 -0700

    Add type annotations to Rfun and R.

--- ToyReals.v
+++ ToyReals.v
@@ -1,6 +1,7 @@
 Require Import QArith.
 
-Definition Rfun := positive -> Z.
+Definition Rfun : Set :=
+  positive -> Z.
 
 Definition Rfun_le (f g : Rfun) : Prop :=
   forall x y, (f x - 1) # x < (g y + 1) # y.
@@ -11,7 +12,8 @@ Definition Rfun_eq (f g : Rfun) : Prop :=
 Definition is_valid_Rfun (f : Rfun) : Prop :=
   Rfun_le f f.
 
-Definition R := {f | is_valid_Rfun f}.
+Definition R : Set :=
+  {f | is_valid_Rfun f}.
 
 Definition Rfun_plus (f g : Rfun) : Rfun :=
   fun x => ((f (4 * x)%positive + g (4 * x)%positive + 2) / 4)%Z.

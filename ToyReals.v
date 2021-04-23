52a3e9ce387da69986917d08c36ddd8cda4dbff5
ccd0 <admin@containerchan.org>
2021-04-23 09:31:33 -0700

    Definition of addition.

--- ToyReals.v
+++ ToyReals.v
@@ -6,3 +6,6 @@ Definition is_valid_Rfun f :=
   forall x y, (f x - 1) # x < (f y + 1) # y.
 
 Definition R := {f | is_valid_Rfun f}.
+
+Definition Rfun_plus (f g : Rfun) : Rfun :=
+  fun x => ((f (4 * x)%positive + g (4 * x)%positive + 2) / 4)%Z.

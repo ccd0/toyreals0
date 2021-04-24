13f1a7d3121299ed5c6dda6efa2ed66fc737fcbc
ccd0 <admin@containerchan.org>
2021-04-23 21:47:16 -0700

    Define "less than or equal" and related propositions on R.

--- ToyReals.v
+++ ToyReals.v
@@ -6,14 +6,20 @@ Definition Rfun : Set :=
 Definition Rfun_le (f g : Rfun) : Prop :=
   forall x y, (f x - 1) # x < (g y + 1) # y.
 
-Definition Rfun_eq (f g : Rfun) : Prop :=
-  Rfun_le f g /\ Rfun_le g f.
-
 Definition is_valid_Rfun (f : Rfun) : Prop :=
   Rfun_le f f.
 
 Definition R : Set :=
   {f | is_valid_Rfun f}.
+
+Definition Rle (x y : R) : Prop :=
+  Rfun_le (proj1_sig x) (proj1_sig y).
+
+Definition Rge (x y : R) : Prop :=
+  Rle y x.
+
+Definition Req (x y : R) : Prop :=
+  Rle x y /\ Rle y x.
 
 Definition Rfun_plus (f g : Rfun) : Rfun :=
   fun x => ((f (4 * x)%positive + g (4 * x)%positive + 2) / 4)%Z.

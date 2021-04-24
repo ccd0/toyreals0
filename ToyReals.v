171efacdf968c0614f929fec0e6162d036ed0255
ccd0 <admin@containerchan.org>
2021-04-23 21:54:36 -0700

    Define "less than" and related propositions.

--- ToyReals.v
+++ ToyReals.v
@@ -6,6 +6,9 @@ Definition Rfun : Set :=
 Definition Rfun_le (f g : Rfun) : Prop :=
   forall x y, (f x - 1) # x < (g y + 1) # y.
 
+Definition Rfun_lt (f g : Rfun) : Prop :=
+  exists x y, (f x + 1) # x <= (g y - 1) # y.
+
 Definition is_valid_Rfun (f : Rfun) : Prop :=
   Rfun_le f f.
 
@@ -20,6 +23,15 @@ Definition Rge (x y : R) : Prop :=
 
 Definition Req (x y : R) : Prop :=
   Rle x y /\ Rle y x.
+
+Definition Rlt (x y : R) : Prop :=
+  Rfun_lt (proj1_sig x) (proj1_sig y).
+
+Definition Rgt (x y : R) : Prop :=
+  Rlt y x.
+
+Definition Rneq (x y : R) : Prop :=
+  Rlt x y \/ Rlt y x.
 
 Definition Rfun_plus (f g : Rfun) : Rfun :=
   fun x => ((f (4 * x)%positive + g (4 * x)%positive + 2) / 4)%Z.

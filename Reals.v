d3f9d968061bff0d755dcf45f73e05d59c4e505b
ccd0 <admin@containerchan.org>
2021-12-24 12:27:35 -0800

    Definition of real number type in nested interval model.

---
+++ Reals.v
@@ -0,0 +1,26 @@
+Require Import Coq.QArith.QArith.
+Require Import Coq.Lists.Streams.
+Global Close Scope Q_scope.
+Local Close Scope nat_scope.
+
+Record Qinterval : Set := make_Qinterval {
+  min : Q;
+  max : Q;
+}.
+
+Declare Scope QI_scope.
+Delimit Scope QI_scope with QI.
+Bind Scope QI_scope with Qinterval.
+Notation "[ a , b ]" := (make_Qinterval a b) (at level 2) : QI_scope.
+Notation "s .[ k ]" := (Str_nth k s) (at level 2, format "s .[ k ]") : QI_scope.
+
+Definition width (xs : Qinterval) : Q :=
+  max xs - min xs.
+
+Record R : Set := make {
+  bounds : Stream Qinterval;
+  bounds_min_le_max : forall k, (min bounds.[k] <= max bounds.[k])%Q;
+  bounds_stricter_min : forall k1 k2, (k2 >= k1)%nat -> (min bounds.[k2] >= min bounds.[k1])%Q;
+  bounds_stricter_max : forall k1 k2, (k2 >= k1)%nat -> (max bounds.[k2] <= max bounds.[k1])%Q;
+  bounds_convergent : (forall eps, eps > 0 -> exists k, width bounds.[k] < eps)%Q;
+}.

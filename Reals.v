8fb3674b7a71b38ac9dea0c0a1de46424edd5492
ccd0 <admin@containerchan.org>
2021-12-25 20:20:02 -0800

    Also label this one a lemma.

--- Reals.v
+++ Reals.v
@@ -63,7 +63,7 @@ Proof.
   apply bounds_min_le_max.
 Qed.
 
-Theorem bounds_nested_elem: forall (x : R) k1 k2 r, (k2 >= k1)%nat -> r ∈ x.[k2] -> r ∈ x.[k1].
+Lemma bounds_nested_elem: forall (x : R) k1 k2 r, (k2 >= k1)%nat -> r ∈ x.[k2] -> r ∈ x.[k1].
 Proof.
   intros x k1 k2 r Hk [H1 H2].
   pose (bounds_stricter_min x k1 k2 Hk).

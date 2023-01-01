93497248e92010995fed9d0522ea58f73ffeee54
ccd0 <admin@containerchan.org>
2023-01-01 14:18:25 -0800

    Use zarith_stubs_js as package and look up package location.

--- extract.sh
+++ extract.sh
@@ -2,4 +2,5 @@
 coqc -R . ToyReals Reals.v
 coqtop -R . ToyReals -batch -l extract.v
 ocamlfind ocamlc -package js_of_ocaml -package js_of_ocaml-ppx -package zarith -linkpkg -o toyreals.byte toyreals.mli toyreals.ml export.ml
-js_of_ocaml ../zarith_stubs_js/biginteger.js ../zarith_stubs_js/runtime.js exportjs.js toyreals.byte
+zsj=$(ocamlfind query zarith_stubs_js)
+js_of_ocaml "$zsj/biginteger.js" "$zsj/runtime.js" exportjs.js toyreals.byte

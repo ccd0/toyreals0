241a7748b068a0abc1b5e15fc1a722c5ba340756
ccd0 <admin@containerchan.org>
2023-01-22 23:01:41 -0800

    Make it work in somewhat older browsers.

--- ui.js
+++ ui.js
@@ -3,7 +3,7 @@ const input = $('textarea');
 const button = $('button');
 const inputDigits = $('input[type="number"]');
 const output = $('output');
-let result;
+var result;
 
 function compute() {
   try {
@@ -17,7 +17,8 @@ function compute() {
 function to_digits(x, n) {
   const exp = R.Z2R(bigInt(10).pow(n));
   const num = bigInt(R.round(R.mult(x, exp)));
-  const s = num.abs().toString().padStart(n + 1, '0');
+  const s1 = num.abs().toString();
+  const s = Array(Math.max(n + 1 - s1.length, 0) + 1).join('0') + s1;
   const i = s.length - n;
   return (num.lt(0) ? '-' : '') + s.substr(0, i) + '.' + s.substr(i);
 }
@@ -26,9 +27,9 @@ function display() {
   try {
     const n_digits = parseInt(inputDigits.value, 10);
     if (result && result.t === 'R') {
-      output.value = `\u2248 ${to_digits(result, n_digits)}`;
+      output.value = '\u2248 ' + to_digits(result, n_digits);
     } else if (result && result.t === 'RI') {
-      output.value = `min \u2248 ${to_digits(result.min, n_digits)}\nmax \u2248 ${to_digits(result.max, n_digits)}`;
+      output.value = 'min \u2248 ' + to_digits(result.min, n_digits) + '\nmax \u2248 ' + to_digits(result.max, n_digits);
     } else if (typeof result === 'function') {
       output.value = '[function]';
     } else if (typeof result === 'undefined') {
